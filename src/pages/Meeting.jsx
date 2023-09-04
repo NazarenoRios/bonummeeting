19818f6a3f4b6762d34a2aa38388678c761daaff
NazarenoRios <nazarenolrios@gmail.com>
2023-09-04 02:08:18 -0300

    button

--- src/pages/Meeting.jsx
+++ src/pages/Meeting.jsx
@@ -2,24 +2,12 @@ import React, { useEffect, useRef, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 function Meeting() {
-  const { meetingId } = useParams();
-
-  const domain = "bonum-meet.bonumcoaching.com"; // Cambia esto al dominio correcto
-  const parentNode = useRef(null);
-  const api = useRef(null);
-  const navigate = useNavigate();
-
-  const isCoachee = false;
-  const user = { name: "namee", lastname: "lastname", languages: ["es", "en"] };
-
-  const [meetingStarted, setMeetingStarted] = useState(false); // Estado para controlar si la reunión ha comenzado
-
   // listener to receive msgs from react native
   useEffect(() => {
     const messageListener = window.addEventListener(
       "message",
       (nativeEvent) => {
-        console.log(nativeEvent?.data);
+        console.log("YAHALLO WEB");
       }
     );
     return messageListener;
@@ -27,76 +15,17 @@ function Meeting() {
 
   // method to send msg to react native
   const sendMessage = () => {
-    window.ReactNativeWebView.postMessage("yahallo! web");
+    window.ReactNativeWebView.postMessage("Hi from PWA");
   };
 
-  useEffect(() => {
-    if (!meetingStarted) {
-      // Evita que se vuelva a iniciar la reunión si ya ha comenzado
-      parentNode.current.style.position = "fixed";
-      parentNode.current.style.top = 0;
-      parentNode.current.style.left = 0;
-      parentNode.current.style.width = "100%";
-      parentNode.current.style.height = "100%";
-      parentNode.current.style.background = "rgba(0, 0, 0, 0.5)";
-      parentNode.current.style.zIndex = 9999;
-      parentNode.current.style.display = "flex";
-      parentNode.current.style.justifyContent = "center";
-      parentNode.current.style.alignItems = "center";
-
-      const options = {
-        roomName: `${meetingId}`,
-        width: "100%",
-        height: "100%",
-        parentNode: parentNode.current,
-        lang: user?.languages[0],
-        userInfo: {
-          displayName: `${user?.name} ${user?.lastname}`,
-        },
-        configOverwrite: {
-          prejoinPageEnabled: false,
-          disableDeepLinking: true,
-          enableClosePage: false,
-          buttonsWithNotifyClick: ["hangup", "hangup-menu"],
-        },
-        interfaceConfigOverwrite: { end_conference: false },
-      };
-
-      api.current = new window.JitsiMeetExternalAPI(domain, options);
-
-      api.current.addEventListener("participantJoined", (event) => {
-        sendMessage();
-
-        // if (isCoachee) {
-        //   api.current.executeCommand("grantModerator", event.id);
-        // }
-      });
-
-      api.current.addEventListener("toolbarButtonClicked", (e) => {
-        // if (isCoachee) {
-        //   return;
-        // }
-
-        // api.current.executeCommand("endConference");
-
-        // const buttonPressed = e.key;
-
-        // if (buttonPressed === "hangup-menu" || buttonPressed === "hangup") {
-        //   api.current.executeCommand("hangup");
-        // }
-        sendMessage();
-      });
-
-      api.current.addEventListener("readyToClose", () => {
-        // navigate("/");
-        sendMessage();
-      });
-
-      setMeetingStarted(true); // Marca la reunión como iniciada
-    }
-  }, [meetingId, meetingStarted, navigate, isCoachee, user, domain]);
-
-  return <div ref={parentNode}></div>;
+  return (
+    <div className="App">
+      <header>
+        <h1>My App Header</h1>
+      </header>
+      <button onClick={sendMessage}>{`Say Hi`}</button>
+    </div>
+  );
 }
 
 export default Meeting;
